6aab651fd6024ea049f47dd2fe142d133a2c99d9
nofprog <k.jin3217@gmail.com>
2021-04-08 23:27:15 +0900

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by J on 2021/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
