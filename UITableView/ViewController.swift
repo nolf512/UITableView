3a3205a736b81d0ab0c3bca9fb5a28c47a8678b5
nofprog <k.jin3217@gmail.com>
2021-04-08 23:51:34 +0900

    tableView構築

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var cellTextArray = ["1", "2", "3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+     
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = cellTextArray[indexPath.row]
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellTextArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell tapped: ", cellTextArray[indexPath.row])
     }
 
 
