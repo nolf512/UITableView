a976278366bb0239e1b1fe9dae162ac27d8cb04a
nofprog <k.jin3217@gmail.com>
2021-04-12 19:27:28 +0900

    rightBarButtonItem, cellの追加メソッド

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var cellTextArray = ["1", "2", "3"]
-
+    var cellTextArray = ["Hello"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNum))
      
+    }
+    
+    @objc func addNum(){
+        
+        let ac = UIAlertController(title: "テキストを入力", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "追加", style: .default, handler: {[weak self, weak ac] _ in
+            guard let newText = ac?.textFields?[0].text else {return}
+            self?.cellTextArray.append(newText)
+            print("addNum: \(newText)") //デバッグ
+            
+            
+            self?.tableView.reloadData()
+            
+        }))
+        
+        present(ac, animated: true, completion: nil)
+
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
